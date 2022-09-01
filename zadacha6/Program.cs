8326ee3f0634a997ee2e78633d8a80788a8fc314
igor <igor.pravdin163@gmail.com>
2022-09-01 20:55:02 +0400

    задача 6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num%2==0)
+{
+Console.WriteLine("ДА");    
+}
+else
+{
+Console.WriteLine("НЕТ");     
+}
